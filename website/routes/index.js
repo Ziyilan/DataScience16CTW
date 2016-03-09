967e8152b60d21acedc6abba289930ecc4a83b41
Ziyi Lan <ziyi.lan@students.olin.edu>
2016-03-09 16:44:29 -0500

    added api

--- website/routes/index.js
+++ website/routes/index.js
@@ -15,7 +15,7 @@ router.question = function(req, res, next) {
 };
 
 router.search = function(rqe, res, next){
-	var url = "http://api.nytimes.com/svc/search/v2/articlesearch.json?q=sleep&page=1&sort=oldest&api-key=a25ad4c53965626d48d407fad419435c:5:74652802";
+	var url = "http://api.nytimes.com/svc/search/v2/articlesearch.json?q=sleep%20deprive%20health&page=1&sort=newest&api-key=a25ad4c53965626d48d407fad419435c:5:74652802";
 	request(url, function(error, response, body){
 	var parse = JSON.parse(body);
 	console.log(parse);
