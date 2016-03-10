77f76b6b97b8fe2495541ff0c4267365f093aa8d
Ziyi Lan <ziyi.lan@students.olin.edu>
2016-03-09 21:09:11 -0500

    parsed url

--- website/routes/index.js
+++ website/routes/index.js
@@ -16,11 +16,21 @@ router.question = function(req, res, next) {
 };
 
 router.search = function(rqe, res, next){
-	var url = "http://api.nytimes.com/svc/search/v2/articlesearch.json?q=sleep&section_name=Science&type_of_material=Blog&page=0&api-key="+auth.NYTIMES_API_KEY;
+	// var url = "http://api.nytimes.com/svc/search/v2/articlesearch.json?q=sleep&section_name=Science&type_of_material=Blog&page=0&api-key="+auth.NYTIMES_API_KEY;
+	var url = "http://api.nytimes.com/svc/search/v2/articlesearch.json?q=sleeping-in-feels-so-good-but-may-be-unhealthy&page=0&api-key="+auth.NYTIMES_API_KEY;
+
 	request(url, function(error, response, body){
 	var parse = JSON.parse(body);
 	console.log(parse);
-	res.json(parse);
+	// res.json(parse.response.docs);
+	// res.json(parse.response.docs[0].web_url);
+	// res.json(parse.response.docs[2].web_url);
+	// res.json(parse.response.docs[3].web_url);
+
+	res.json(parse.response.docs[0].web_url);
+
+
+	// res.json(Object.keys(parse.response.docs[1].web_url));
 	});
 };
 
